f8c7809d438232f8437dd49ae9fd76fe57832e6d
Ares <109671478+0xAresDev@users.noreply.github.com>
2022-11-01 15:15:30 +0100

    Fixed the fixes for main.py

--- TrendBasedTradingBot/Code/scripts/StoreData/InitializeDB.py
+++ TrendBasedTradingBot/Code/scripts/StoreData/InitializeDB.py
@@ -4,48 +4,68 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import Column, Integer, DateTime, desc, asc
 import pathlib
 import os
-import datetime
+import datetime, time
+from .InitializeDB import Price, SQLALCHEMY_DATABASE_URI
+#import .InitializeDB
+#import .UniswapData
+from .UniswapData import get_uniswap_price
+import sched
+
+eth = "0x0000000000000000000000000000000000000000"
+dai = "0x6B175474E89094C44Da98b954EedeAC495271d0F"
 
 
-# Run this file to create the DB and the tables...
+def main():
 
-#SQLALCHEMY_DATABASE_URI = "sqlite:///PriceData.db"
-directory_for_db = pathlib.Path(__file__).parent.resolve()
-SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(directory_for_db, 'PriceData.db')}"
+    s = sched.scheduler(time.time, time.sleep)
 
-Base = declarative_base()
+    def main_func(sc):
+        timestamp = datetime.datetime.now()
+        price = get_uniswap_price(eth, dai)
+        print(price)
+        add_to_db(price, timestamp)
+    
+        sc.enter(60*60*24, 1, main_func, (sc,))
 
-class Price(Base):
-    __tablename__ = 'price'
-
-    id = Column(Integer, primary_key=True)
-    price = Column(Float)
-    time = Column(DateTime)
-
-    def __repr__(self):
-        return f'Time: {self.time} Price: {self.price}'
+    s.enter(60*60*24, 1, main_func, (s,))
+    s.run()
     
 
-    @classmethod
-    def get_last_x_prices(cls, session, x):
-        return session.query(cls).order_by(desc(cls.time)).limit(x).all()
-    
 
-
-def initialize_db(test=False):
-    engine = None
+def add_to_db(price, timestamp, test=False):
+    engine=None
     if test:
-            engine = create_engine(SQLALCHEMY_DATABASE_URI+"_test", echo=True)
+        engine = create_engine(SQLALCHEMY_DATABASE_URI+"_test", echo=True)
+
     else:
         engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=True)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    Base.metadata.create_all(engine)
-
+    exchange_rate = Price(price=price, time=timestamp, id=None)
+    session.add(exchange_rate)
     session.commit()
     session.close()
 
+# should give you x last data points back (sorted by date and time)
+def read_data_from_db(x, test=False):
+    engine=None
+    if test:
+        engine = create_engine(SQLALCHEMY_DATABASE_URI+"_test", echo=True)
+
+    else:
+        engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    price_data = [(x.price, x.time) for x in Price.get_last_x_prices(session, x)]
+    session.close()
+    return price_data
+
+
+
 if __name__ == "__main__":
-    initialize_db()
+    main()
+    #print(read_data_from_db(10))
