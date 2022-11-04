9a94cc6ef3757d5e8e4c3a5fe4eca327449a50c0
Ares <109671478+0xAresDev@users.noreply.github.com>
2022-11-04 12:14:18 +0100

    Update test_Interactions.py

--- TrendBasedTradingBot/Code/tests/test_Interactions.py
+++ TrendBasedTradingBot/Code/tests/test_Interactions.py
@@ -6,13 +6,14 @@ import time
 
 trading_v6_testnet = "0x81465dF3c64B18b4092990eB73200A3814AF75E5"
 my_account = "0xc0Ea5c77570efDD46911436bdb295cAc57720a31"
+account1 = accounts.add(config["wallets"]["wallet1"])
+BTC_price = 20621.73
 
 def test_open_and_close_trade():
     # Test for BTC/DAI
-    events = open_trade(0, 19000, 1000, True, 10, 25, 10, 3, my_account)
+    #account, pairIndex, price, position_size, buy_trade, leverage, take_profit, stop_loss, slippage, referrer=my_account
+    events = open_trade(account1, 0, BTC_price, 1000, True, 10, 25, 10, 3, my_account)
     assert "MarketOrderInitiated" in events.keys() 
     time.sleep(10)
-    events = close_trade(0, 0)
+    events = close_trade(account1, 0, 0)
     assert "MarketOrderInitiated" in events.keys() 
-
-
